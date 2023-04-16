289a9488b710c3a9f1203670706628e56057b41d
dongyshi <dongyshi@student.42seoul.fr>
2023-04-16 17:49:41 +0900

    [FEAT] 1. expand_filename에 필요한 구현들 작성 완료(테스트 x) 2. get_path.c오류 있던 부분 bug fix했음.

--- srcs/parse/get_path.c
+++ srcs/parse/get_path.c
@@ -3,97 +3,86 @@
 /*                                                        :::      ::::::::   */
 /*   get_path.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
+/*   By: dongyshi <dongyshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 16:24:01 by dongyshi          #+#    #+#             */
-/*   Updated: 2023/04/14 21:42:51 by seunghoy         ###   ########.fr       */
+/*   Updated: 2023/04/16 17:43:06 by dongyshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "minishell.h"
-// #include "libft.h"
-// #include <stdio.h>
-// #include <stdlib.h>
+#include "minishell.h"
+#include "parse.h"
+#include "libft.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-// static void	make_path(char **path, char *wild_card_str, int path_len, int add_len);
+static void	make_path(char **path, char *wild_card_str, int path_len, int add_len);
 
-// char	*get_path(char *wild_card_str)
-// {
-// 	int		i;
-// 	int		path_len;
-// 	int		add_len;
-// 	char	*path;
+char	*get_path(char *wild_card_str) // 무조건 별이 들어오는 부분.
+{
+	int		i;
+	int		path_len;
+	int		add_len;
+	char	*path;
 
-// 	if (wild_card_str[0] != '/')
-// 		return (NULL);
-// 	path = NULL;
-// 	i = -1;
-// 	add_len = 0;
-// 	path_len = 0;
-// 	while (wild_card_str[++i])
-// 	{
-// 		if (wild_card_str[i] == '*')
-// 			break ;
-// 		if (wild_card_str[i] == '/')
-// 		{
-// 			path = ft_substr(wild_card_str, 0, path_len + add_len + 1);
-// 			path_len = ft_strlen(path);
-// 			add_len = 0;
-// 			i++;
-// 		}
-// 		++add_len;
-// 	}
-// 	return (path);
-// }
+	if (wild_card_str[0] != '/')
+		return (NULL);
+	path = NULL;
+	i = 0;
+	add_len = 0;
+	path_len = 0;
+	while (wild_card_str[i])
+	{
+		if (wild_card_str[i] == '*')
+			break ;
+		if (wild_card_str[i] == '/')
+		{
+			path = ft_substr(wild_card_str, 0, path_len + add_len + 1);
+			path_len = ft_strlen(path);
+			add_len = 0;
+		}
+		else
+			add_len++;
+		i++;
+	}
+	return (path);
+}
 
-// void	get_pattern(/*t_admin *pattern_list, */int path_len, char *wild_card_str)
-// {
-// 	int	start_idx;
-// 	int	end_idx;
+void	get_pattern(t_token_list *pattern_list, int path_len, char *wild_card_str)
+{
+	int	start_idx;
+	int	end_idx;
 
-// 	start_idx = path_len;
-// 	end_idx = start_idx;
-// 	while (1)
-// 	{
-// 		if (wild_card_str[end_idx] == '/')
-// 		{
-// 			printf("%s\n", ft_substr(wild_card_str, start_idx, end_idx - start_idx + 1));
-// 			start_idx = end_idx + 1;
-// 		}
-// 		else if (wild_card_str[end_idx] == 0)
-// 		{
-// 			printf("%s\n", ft_substr(wild_card_str, start_idx, end_idx - start_idx + 1));
-// 			break ;
-// 		}
-// 		++end_idx;
-// 	}
-// }
-// // static void	make_path(char **path, char *wild_card_str, int path_len, int add_len)
-// // {
-// // 	if (*path == NULL)
-// // 	{
-// // 		*path = (char *)malloc(sizeof(char) * 2);
-// // 		if (*path == NULL)
-// // 			malloc_error();
-// // 		(*path)[0] = '/';
-// // 		(*path)[1] = '\0';
-// // 	}
-// // 	else
-// // 	{
-// // 		free(*path);
-// // 		*path = (char *)malloc(sizeof(char) * (path_len + add_len + 1));
-// // 		if (*path == NULL)
-// // 			malloc_error();
-// // 		ft_strlcpy((*path), wild_card_str, path_len + add_len + 1);
-// // 	}
-// // }
+	start_idx = path_len;
+	end_idx = start_idx;
+	while (1)
+	{
+		if (wild_card_str[end_idx] == '/')
+		{
+			add_token(pattern_list, ft_substr(wild_card_str, start_idx, end_idx - start_idx + 1));
+			start_idx = end_idx + 1;
+		}
+		else if (wild_card_str[end_idx] == 0)
+		{
+			add_token(pattern_list, ft_substr(wild_card_str, start_idx, end_idx - start_idx + 1));
+			pattern_list->tail->next = NULL;
+			break ;
+		}
+		++end_idx;
+	}
+}
 
-// int main(void)
-// {
-// 	char	*str = "/AAA/BBB/CCC/*/E*/F";
-// 	char	*path = get_path(str);
+int main(void)
+{
+	char	*str = "/AAA/BB/***/*D/*/**";
+	char	*path = get_path(str);
+	t_token_list	*pattern;
 
-// 	printf("%s\n", path);
-// 	get_pattern(ft_strlen(path), str);
-// 	return (0);
-// }
+	printf("%s\n", path);
+	get_pattern(pattern, ft_strlen(path), str);
+	t_token *head = pattern->head;
+	while (head != NULL) {
+		printf("%s\n", head->string);
+	}
+	return (0);
+}
