97e28240b295083b88cee734f89a0dd8c4691999
pumple99 <ljhfree99@naver.com>
2023-04-20 20:13:57 +0900

    feat/execute_pipe

--- includes/execute.h
+++ includes/execute.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dongyshi <dongyshi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 18:57:12 by dongyshi          #+#    #+#             */
-/*   Updated: 2023/04/20 19:08:20 by dongyshi         ###   ########.fr       */
+/*   Updated: 2023/04/20 19:45:37 by seunghoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 #include "list.h"
 #include "parse.h"
+
+typedef struct s_fds
+{
+	int	left[2];
+	int	right[2];
+}	t_fds;
 
 typedef enum e_builtin_num
 {
@@ -29,6 +35,7 @@ typedef enum e_builtin_num
 
 //execute_minimal.c
 int	execute_minimal(t_admin *hash_map, char ***envp, t_token *token, int is_pipe);
+
 //execute_minimal_utils.c
 char	*has_paren(t_token *token);
 int		is_builtin(char *minimal_cmd);
