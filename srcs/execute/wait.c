b3af4beda58273273d444c8e1ebf1968a5d9c68e
dongyshi <dongyshi@student.42seoul.fr>
2023-04-21 19:54:25 +0900

    feat/implement_wait_func

--- srcs/execute/wait.c
+++ srcs/execute/wait.c
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   wait.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
+/*   By: dongyshi <dongyshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 19:19:45 by seunghoy          #+#    #+#             */
-/*   Updated: 2023/04/21 19:20:34 by seunghoy         ###   ########.fr       */
+/*   Updated: 2023/04/21 19:52:13 by dongyshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "list.h"
+#include "libft.h"
 #include <stdlib.h>
+
+static void	set_exit_status(int child_status, int *exit_status);
+static void	set_questionmark(t_admin *hash_map, int exit_status);
+
+void	wait_last_child(t_admin *hash_map, pid_t last_child_pid)
+{
+	int		exit_status;
+	int		child_status;
+	pid_t	pid_num;
+
+	pid_num = 0;
+	while (pid_num != -1)
+	{
+		pid_num = wait(&child_status);
+		if (pid_num == last_child_pid)
+		{
+			set_exit_status(child_status, &exit_status);
+			set_questionmark(hash_map, exit_status);
+		}
+	}
+}
 
 static void	set_exit_status(int child_status, int *exit_status)
 {
@@ -24,4 +47,17 @@ static void	set_exit_status(int child_status, int *exit_status)
 	else if (WIFSTOPPED(child_status))
 		status = WSTOPSIG(child_status);
 	*exit_status = status;
+}
+
+static void	set_questionmark(t_admin *hash_map, int exit_status)
+{
+	char	*exit_status_char;
+	char	*prev_exit_status_char;
+
+	exit_status_char = ft_itoa(exit_status);
+	prev_exit_status_char = exit_status_char;
+	exit_status_char = ft_strjoin("?=", exit_status_char);
+	add_node(hash_map, exit_status_char);
+	free(exit_status_char);
+	free(prev_exit_status_char);
 }
