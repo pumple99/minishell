8be3a10ea2c9573a2c4e4d0e1fb512f800c32381
dongyshi <dongyshi@student.42seoul.fr>
2023-04-22 20:43:22 +0900

    feat/safe함수들 구현 완료

--- includes/safe_function.h
+++ includes/safe_function.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   safe_function.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
+/*   By: dongyshi <dongyshi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:39:05 by seunghoy          #+#    #+#             */
-/*   Updated: 2023/04/21 18:34:25 by seunghoy         ###   ########.fr       */
+/*   Updated: 2023/04/22 20:43:10 by dongyshi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,18 @@ void	perr_exit(char *str);
 int		dup_s(int fd);
 int		dup2_s(int fd1, int fd2);
 int		close(fd);
+
+
+//use_dir_safe.c
+int				chdir_s(const char *path);
+int				closedir_s(DIR *dirp);
+struct dirent	*readdir_s(DIR *dirp);
+
+//use_heap_safe.c
+void			*malloc_s(int size_of_container);
+
+//use_write_getcwd_safe.c
+char			*getcwd_s(char *buf, size_t size);
+int				write_s(int fd, const char *str, int _strlen);
 
 #endif
---
+++ srcs/err_handle/use_write_getcwd_safe.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   use_write_getcwd_safe.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongyshi <dongyshi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/22 20:07:20 by dongyshi          #+#    #+#             */
+/*   Updated: 2023/04/22 20:18:47 by dongyshi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "../../includes/safe_function.h"
+
+char	*getcwd_s(char *buf, size_t size)
+{
+	char	*re;
+
+	re = getcwd(buf, size);
+	if (re != NULL)
+		return (re);
+	perror("minishell: getcwd failed");
+}
+
+int	write_s(int fd, const char *str, int _strlen)
+{
+	int	re;
+
+	re = write(fd, str, _strlen);
+	if (re == _strlen)
+		return (re);
+	perror("minishell: write failed");
+}
