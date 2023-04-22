4f12753df866215c3d7f5ce12647857df6fcba54
dongyshi <dongyshi@student.42seoul.fr>
2023-04-22 21:53:06 +0900

    refactor/cd_refactoring

--- includes/builtin.h
+++ includes/builtin.h
@@ -3,6 +3,7 @@
 
 #include "list.h"
 
+//builtin
 int		builtin_env(char ***envp);
 int		builtin_export(t_admin *hash_map, char ***envp, char **export_args);
 int		builtin_unset(t_admin *hash_map, char ***envp, char **args);
@@ -10,7 +11,21 @@ int		builtin_exit(char **args);
 int		builtin_cd(t_admin *hash_map, char ***envp, char **args);
 int		builtin_pwd(t_admin *hash_map);
 int		builtin_echo(char **args);
+
+//cd_find_path.c
+char	*make_path(t_admin *hash_map, char *arg);
+char	*using_cdpath(t_admin *hash_map, char *arg);
+
+//cd_change_pwd_oldpwd.c
+void	changing_env(t_admin *hash_map, char *path_to_move, char *prev_pwd);
+
+//exit_utils.c
+long long	ft_atol(const char *str, int *flag);
+
+//make_hash_map.c
 void	make_hash_map(t_admin *hash_map, char *envp[], char ***new_envp);
+
+//make_new_envp.c
 void	make_new_envp(t_admin *hash_map, char ***envp);
 
 #endif
