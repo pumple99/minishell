11116208c999d195ddb27b03e23e96dbb24d2475
seunghoy <ljhfree99@naver.com>
2023-04-22 20:14:02 +0900

    fix/execute_pipe.c

--- srcs/execute/execute_pipe.c
+++ srcs/execute/execute_pipe.c
@@ -6,7 +6,7 @@
 /*   By: seunghoy <seunghoy@student.42.kr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 21:24:27 by seunghoy          #+#    #+#             */
-/*   Updated: 2023/04/21 19:16:40 by seunghoy         ###   ########.fr       */
+/*   Updated: 2023/04/22 20:10:42 by seunghoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,14 @@ static void	close_left_and_move(int *l, int *r, int count)
 {
 	if (count != 1)
 	{
-		check_close(l[0]);
-		check_close(l[1]);
+		close_s(l[0]);
+		close_s(l[1]);
 	}
 	l[0] = r[0];
 	l[1] = r[1];
 }
 
-t_token	*get_next_pipe(t_token *token)
+static t_token	*get_next_pipe(t_token *token)
 {
 	int		paren_depth;
 
@@ -45,57 +45,58 @@ t_token	*get_next_pipe(t_token *token)
 	}
 }
 
-void	fd_change(int pipe_count, t_fds *fds)
+static void	fd_change(int child_count, t_fds *fds)
 {
-	if (pipe_count != 1)
+	if (child_count != 1)
 	{
-		check_close((fds->left)[1]);
-		check_dup2((fds->left)[0], STDIN);
+		close_s((fds->left)[1]);
+		dup2_s((fds->left)[0], STDIN_FILENO);
 	}
-	if (pipe_count != 0)
+	if (child_count != 0)
 	{
-		check_close((fds->right)[0]);
-		check_dup2((fds->right)[1], STDOUT);
+		close_s((fds->right)[0]);
+		dup2_s((fds->right)[1], STDOUT_FILENO);
 	}
 }
 
-int	make_pipe_and_execute_minimal(t_token *token, int pipe_total)
+static int	pipe_connect_execute(t_admin *hash_map, char ***envp, \
+t_token *token, int child_total)
 {
 	pid_t	pid;
-	int		pipe_count;
+	int		child_count;
 	t_fds	fds;
 
-	pipe_count = 0;
+	child_count = 0;
 	pid = 1;
-	while (pid != 0 && ++pipe_count <= pipe_total)
+	while (pid != 0 && ++child_count <= child_total)
 	{
-		if (pipe_count != 1)
+		if (child_count != 1)
 			token = get_next_pipe(token);
-		if (pipe_count != pipe_total)
+		if (child_count != child_total)
 			check_pipe(fds.left);
 		pid = check_fork();
 		if (pid != 0)
-			close_left_and_move(fds.left, fds.right, pipe_count);
+			close_left_and_move(fds.left, fds.right, child_count);
 	}
-	if (pid == 0 && pipe_count < pipe_total)
-		fd_change(pipe_count, &fds);
-	else if (pid == 0 && pipe_count == pipe_total)
+	if (pid == 0 && child_count < child_total)
+		fd_change(child_count, &fds);
+	else if (pid == 0 && child_count == child_total)
 		fd_change(0, &fds);
 	if (pid == 0)
-		return (exit(execute_minimal()));
+		return (exit(execute_minimal(hash_map, envp, token, 1)), 0);
 	else
-		return (wait());
+		return (wait_last_child(hash_map, pid, child_total));
 }
 
-int	execute_pipe(t_token *token)
+int	execute_pipe(t_admin *hash_map, char ***envp, t_token *token)
 {
-	int		pipe_count;
+	int		child_count;
 	int		paren_depth;
 	t_token	*temp;
 
 	paren_depth = 0;
 	temp = token;
-	pipe_count = 0;
+	child_count = 1;
 	while (temp->type != end)
 	{
 		if (paren_depth == 0 && is_or_and_end(temp))
@@ -105,11 +106,11 @@ int	execute_pipe(t_token *token)
 		else if (temp->type == paren_r)
 			--paren_depth;
 		else if (temp->type == _pipe && paren_depth == 0)
-			++pipe_count;
+			++child_count;
 		temp = temp->next;
 	}
-	if (pipe_count == 0)
-		return (execute_minimal(tl, token, 0));
+	if (child_count == 1)
+		return (execute_minimal(hash_map, envp, token, 0));
 	else
-		return (make_pipe_and_execute_minimal(token, pipe_count));
+		return (pipe_connect_execute(hash_map, envp, token, child_count));
 }
